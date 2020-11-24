306f02c0fa64463b1dd40bfa00a4b558088811a1
bankday080 <bankday02@gmail.com>
2020-11-24 16:44:30 +0700

    add1

--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -3,7 +3,7 @@ import User from "../models/user";
 const resolvers = {
   Query: {
     user: (parent, args, context, info) => {return User.findById(args.id)}, /*สำคัญ */
-    users: (parent, arg, context, info) => {return User.findById()},
+    users: (parent, arg, context, info) => {return User.find()},
   },
   Mutation:{
       signup: (parent, args, context, info) => {return User.create(args)}
